4bae9a0672729bd62ed3f875b512d7651bdfb350
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-01 18:07:52 +0900

    BE : JPA코드수정

--- backend/src/main/java/com/sharespot/service/TestService.java
+++ backend/src/main/java/com/sharespot/service/TestService.java
@@ -1,6 +1,7 @@
 package com.sharespot.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import com.sharespot.entity.TestEntity;
 import com.sharespot.repo.TestRepository;
@@ -18,5 +19,27 @@ public class TestService {
 		TestEntity savedTest = testRepository.save(testEntity);
 		return savedTest;
 	}
+	
+	public TestEntity updateMember(TestEntity testEntity) {
+		TestEntity updatedTest = null;
+        try {
+        	TestEntity existTest = getTest(testEntity.getId());
+            if (!ObjectUtils.isEmpty(existTest)) {
+                updatedTest = TestRepository.save(testEntity);  // JpaRepository에서 제공하는 save() 함수
+            }
+        } catch (Exception e) {
+            log.info("[Fail] e: " + e.toString());
+        } finally {
+            return updatedTest;
+        }
+    }
+	
+	public TestEntity getTest(Integer id) {
+        return testRepository.getById(id);  // JpaRepository에서 제공하는 getById() 함수
+    }
+	
+	public void deleteTest(Integer id) {
+        testRepository.deleteById(id);  // JpaRepository에서 제공하는 deleteById() 함수
+    }
 	
 }

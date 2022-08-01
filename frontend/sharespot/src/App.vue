81a0b7a6ff779db30c9ab5bc7b6e87e15c582d99
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-08-01 17:40:03 +0900

    FE: app.vue 기본 폰트 추가

--- frontend/sharespot/src/App.vue
+++ frontend/sharespot/src/App.vue
@@ -15,3 +15,12 @@ export default {
   }),
 };
 </script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Gothic+A1:wght@700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap");
+
+* {
+  font-family: "Nanum Gothic";
+}
+</style>

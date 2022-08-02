fb8076af8d9d5cf507181857cb09dae6f3c355e7
유현욱 <hyundnr1126@naver.com>
2022-08-02 23:15:41 +0900

    FE: setup기본틀 제작

--- frontend/sharespot/src/views/settings/SettingsInfo.vue
+++ frontend/sharespot/src/views/settings/SettingsInfo.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        개인정보변경 설정
-    </div>
+    <v-list class="mx-2">
+        <v-list-item>
+        <v-list-item-title class="mx-2 my-4">도움말</v-list-item-title>
+        <v-icon
+        @click="next"
+        >
+        mdi-chevron-right
+        </v-icon>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+        <v-list-item-title class="mx-2 my-4">이용약관</v-list-item-title>
+        <v-icon
+        @click="next"
+        >
+        mdi-chevron-right
+        </v-icon>
+        </v-list-item>
+    </v-list>
 </template>
 
 <script>
 export default {
-    name: 'SettingsInfo',
+    name: 'S07P12A505SettingsNotice',
 
     data() {
         return {

26a0ebfc3f6444d13a986f17c466311c8930c5a5
chohyean <ashley3053@gmail.com>
2022-08-03 10:37:39 +0900

    FE: HotSpotMenu 화면 구현
    - HotSpotMenu 컴포넌트 구현
    - 해당 메뉴 클릭 시 생성되는 모달 구현
    그 외:
    - font 두께 추가
    - router 프로필조회에 /:userid 추가

--- frontend/sharespot/src/App.vue
+++ frontend/sharespot/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <v-app>
     <v-main>
+      <hot-spot-menu></hot-spot-menu>
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import HotSpotMenu from './components/curation/HotSpotMenu.vue';
+
 export default {
+  components: { HotSpotMenu },
   name: "App",
 
   data: () => ({
@@ -16,7 +20,7 @@ export default {
 };
 </script>
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Gothic+A1:wght@700&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap");
 

41155a1247b2f10957b2359c616beed8bbcc040c
김태웅 <85680155+KimTaeWonng@users.noreply.github.com>
2022-08-03 15:50:15 +0900

    BE : User Update 기능 넣음
    죽여줘

--- backend/src/main/java/com/sharespot/controller/UserController.java
+++ backend/src/main/java/com/sharespot/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,62 +23,74 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@PostMapping("/signup")
-	public ResponseEntity<User> signUp(@RequestBody User user) throws ParseException{
-		User userEntity = User.builder()
-				.user_id(user.getUser_id())
-				.email(user.getEmail())
-				.password(user.getPassword())
-				.phone(user.getPhone())
-				.gender(user.getGender())
-				.birth(user.getBirth())
-				.name(user.getName())
-				.nickname(user.getNickname())
-				.introduce(user.getIntroduce())
-				.profile_image(user.getProfile_image())
-				.BD(user.getBD())  			// isBadge
-				.alarm(user.getAlarm())  	// isalarm
-				.GD(user.getGD()) 			// isgender
-				.BR(user.getBR()) 			// isBirth
-				.PB(user.getPB())		 	// isPublic
-				.user_grade(user.getUser_grade())
-				.build();
-		
+	public ResponseEntity<User> signUp(@RequestBody User user) throws ParseException {
+		User userEntity = User.builder().user_id(user.getUser_id()).email(user.getEmail()).password(user.getPassword())
+				.phone(user.getPhone()).gender(user.getGender()).birth(user.getBirth()).name(user.getName())
+				.nickname(user.getNickname()).introduce(user.getIntroduce()).profile_image(user.getProfile_image())
+				.BD(user.getBD()) // isBadge
+				.alarm(user.getAlarm()) // isalarm
+				.GD(user.getGD()) // isgender
+				.BR(user.getBR()) // isBirth
+				.PB(user.getPB()) // isPublic
+				.user_grade(user.getUser_grade()).build();
+
 		User savedUser = userService.createUser(userEntity);
-		return new ResponseEntity<User>(savedUser,HttpStatus.OK);
+		return new ResponseEntity<User>(savedUser, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/idcheck/{email}")
 	public ResponseEntity<Boolean> checkid(@PathVariable String email) {
-		
+
 		boolean check = userService.idCheck(email);
-		
+
 		return ResponseEntity.ok(userService.idCheck(email));
-		
+
 	}
-	
+
 	@DeleteMapping("{id}")
-	public ResponseEntity<Integer> deleteUser(@PathVariable int id){
+	public ResponseEntity<Integer> deleteUser(@PathVariable int id) {
 		userService.deleteUser(id);
-		
-		return new ResponseEntity<Integer>(id,HttpStatus.OK);
+
+		return new ResponseEntity<Integer>(id, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/logout")
-	public ResponseEntity<Boolean> logout(){
-		
+	public ResponseEntity<Boolean> logout() {
+
 		boolean logout = false;
-		
+
 		// jwt 사용
-		
+
 		//
-		return new ResponseEntity<Boolean>(logout,HttpStatus.OK);
+		return new ResponseEntity<Boolean>(logout, HttpStatus.OK);
 	}
-	
+
+	@PutMapping("{userid}")
+	public ResponseEntity<Integer> modifyUser(@RequestBody User user) {
+
+		User userEntity = User.builder().user_id(user.getUser_id()).email(user.getEmail()).password(user.getPassword())
+				.phone(user.getPhone()).gender(user.getGender()).birth(user.getBirth()).name(user.getName())
+				.nickname(user.getNickname()).introduce(user.getIntroduce()).profile_image(user.getProfile_image())
+				.BD(user.getBD()) // isBadge
+				.alarm(user.getAlarm()) // isalarm
+				.GD(user.getGD()) // isgender
+				.BR(user.getBR()) // isBirth
+				.PB(user.getPB()) // isPublic
+				.user_grade(user.getUser_grade()).build();
+
+		Integer result = userService.modify(user.getEmail(), userEntity);
+			
+		return new ResponseEntity<Integer>(result, HttpStatus.OK);
+		
+			
+		
+	}
+
 //	@PostMapping("/login")
 //	public ResponseEntity<T>
 
--- backend/src/main/java/com/sharespot/service/UserService.java
+++ backend/src/main/java/com/sharespot/service/UserService.java
@@ -3,21 +3,28 @@ package com.sharespot.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.apache.commons.*;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sharespot.entity.User;
 import com.sharespot.repo.UserRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-	@Autowired
-	private UserRepository userRepository;
+	
+	private final UserRepository userRepository;
 
+	@Transactional
 	public User createUser(User user) {
 
 //		if (userRepository.findById(user.getId()) == null) {
@@ -27,6 +34,34 @@ public class UserService {
 //			
 //		}
 	}
+	
+	public int modify(String email, User value) {
+		Optional<User> mUser = userRepository.findByemail(email);
+		if(mUser.isPresent()) {
+			User user = mUser.get();
+			if(StringUtils.isNotBlank(value.getProfile_image())) {
+				user.setProfile_image(value.getProfile_image());
+			}
+			if(StringUtils.isNotBlank(value.getNickname())) {
+				user.setNickname(value.getNickname());
+			}
+			if(StringUtils.isNotBlank(value.getIntroduce())) {
+				user.setIntroduce(value.getIntroduce());
+			}
+			if(StringUtils.isNotBlank(value.getPhone())) {
+				user.setPhone(value.getPhone());
+			}
+			user.setPB(value.getPB());
+			user.setBR(value.getBR());
+			user.setGD(value.getGD());
+			user.setGender(value.getGender());
+			userRepository.save(user);
+			
+			return 1;
+		}
+		return 0;
+	}
+	
 
 	public List<User> getUsers() {
 		return userRepository.findAll();

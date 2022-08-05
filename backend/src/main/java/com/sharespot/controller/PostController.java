cd8ca290b3f147795e145410c534a1d852327d43
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-05 11:37:58 +0900

    BE: Post CRUD

--- backend/src/main/java/com/sharespot/controller/PostController.java
+++ backend/src/main/java/com/sharespot/controller/PostController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +37,7 @@ public class PostController {
 		return new ResponseEntity<Optional<Post>>(post, HttpStatus.OK);
 	}
 	
-	@PostMapping
+	@PostMapping("/posts")
 	public ResponseEntity<Integer> createPost(@RequestBody Post post){
 		Post postEntity = Post.builder()
 			.userId(post.getUserId())
@@ -52,7 +54,31 @@ public class PostController {
 			.build();
 		postRepository.save(postEntity);
 		return new ResponseEntity<Integer>(1, HttpStatus.OK);
-		
 	}
+	@PutMapping("/posts/{postNo}")
+	public ResponseEntity<Integer> updatePost(@RequestBody Post post, @PathVariable int postNo){
+		Optional<Post> option = postRepository.findById(postNo);
+		int result = 0;
+		if(option.isPresent()) { 
+			
+			Post p = option.get();
+
+			p.setContent(post.getContent());
+			p.setPostLat(post.getPostLat());
+			p.setPostLng(post.getPostLng());
+			p.setImage(post.getImage());
+			p.setPostGpsName(post.getPostGpsName());
+			p.setClassBig(post.getClassBig());
+			p.setClassSmall(post.getClassSmall());
+			p.setClassWho(post.getClassWho());
+			p.setClassWhere(post.getClassWhere());
+			postRepository.save(p);
+			
+			result = 1;
+			}
+		
+		return new ResponseEntity<Integer>(result, HttpStatus.OK);
+	}
+	
 	
 }

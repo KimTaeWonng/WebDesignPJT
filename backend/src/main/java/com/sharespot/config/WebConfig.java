87d1dc136e9ccbe22ef100560d327e5e0e0072ea
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-07 20:17:12 +0900

    FIX: Coreseession 주석처리
    문제가 발생하여 주석처리함

--- backend/src/main/java/com/sharespot/config/WebConfig.java
+++ backend/src/main/java/com/sharespot/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.mysql.cj.CoreSession;
+//import com.mysql.cj.CoreSession;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{

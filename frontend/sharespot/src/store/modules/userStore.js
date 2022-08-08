7b53b8ffe72abdac9082edfa7b5c21bc5f1dfe86
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-08-08 17:55:42 +0900

    FE: 로그인 jwt 기능
    userStore, index.js, user.js 추가

---
+++ frontend/sharespot/src/store/modules/userStore.js
@@ -0,0 +1,70 @@
+import jwt_decode from "jwt-decode";
+import { login, findById } from "@/js/user.js";
+
+const userStore = {
+    namespaced: true,
+    state: {
+        isLogin: false,
+        isLoginError: false,
+        userInfo: null,
+    },
+    getters: {
+        checkUserInfo: function (state) {
+            return state.userInfo;
+        },
+    },
+    mutations: {
+        SET_IS_LOGIN: (state, isLogin) => {
+            state.isLogin = isLogin;
+        },
+        SET_IS_LOGIN_ERROR: (state, isLoginError) => {
+            state.isLoginError = isLoginError;
+        },
+        SET_USER_INFO: (state, userInfo) => {
+            if (userInfo != null) {
+                state.isLogin = true;
+            }
+            state.userInfo = userInfo;
+        },
+    },
+    actions: {
+        async userConfirm({ commit }, user) {
+            await login(
+                user,
+                (response) => {
+                    if (response.data.message === "success") {
+                        let token = response.data["Authorization"];
+                        commit("SET_IS_LOGIN", true);
+                        commit("SET_IS_LOGIN_ERROR", false);
+                        console.log(token) // 로그인 유저의 토큰 확인
+                        sessionStorage.setItem("Authorization", token);
+                    } else {
+                        commit("SET_IS_LOGIN", false);
+                        commit("SET_IS_LOGIN_ERROR", true);
+                    }
+                }
+            );
+        },
+        async getUserInfo({ commit }, token) {
+            let decode_token = jwt_decode(token);
+            await findById(
+                decode_token.userid,
+                //console.log(decode_token.userid), // 로그인한 유저의 ID 출력
+                (response) => {
+                    console.log(response);
+                    if (response.data.message === "success") {
+                        commit("SET_USER_INFO", response.data.userInfo);
+                        //console.log(response.data.userInfo);
+                    } else {
+                        console.log("유저 정보 없음");
+                    }
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        },
+    }
+};
+
+export default userStore;
--- frontend/sharespot/src/views/users/Login.vue
+++ frontend/sharespot/src/views/users/Login.vue
@@ -1,80 +1,106 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <div style="margin-top: 30%; margin-left: 25%">
-          <v-img
-            :src="require('/src/assets/logo.png')"
-            class="my-3"
-            contain
-            width="70%"
-            height="70%"
-          />
-        </div>
-
-        <p style="text-align: left; margin-left: 3%">아이디 (이메일)</p>
-        <div style="margin-left: 5%; margin-right: 5%; line-height: 0">
-          <v-textarea
-            label=""
-            auto-grow
-            outlined
-            rows="1"
-            row-height="15"
-          ></v-textarea>
-        </div>
-
-        <br />
-
-        <p style="text-align: left; margin-left: 3%">비밀번호</p>
-        <div style="margin-left: 5%; margin-right: 5%; line-height: 0">
-          <v-textarea
-            label=""
-            auto-grow
-            outlined
-            rows="1"
-            row-height="15"
-          ></v-textarea>
-          <p style="text-align: right;">
+    <v-container>
+        <v-row class="text-center">
+            <div style="margin-top: 20%;">
+                <img src="@/assets/logo.png" style="width: 50%; height: auto;"/>
+            </div>
+        </v-row>
+        <v-row justify="center" v-if="isLoginError">
+          <v-alert
+      dense
+      outlined
+      type="error"
+    >
+      <strong>아이디</strong> 혹은 <strong>비밀번호</strong>를 확인해주세요.
+    </v-alert>
+        </v-row>
+        <v-row class="ml-4 mr-4 mb-4">
+            <v-text-field
+            label="아이디(이메일)"
+            v-model="user.email"
+            class="input-group--focused"
+            color="#289672"
+        ></v-text-field>
+        </v-row>
+        <v-row class="ml-4 mr-4">
+            <v-text-field
+            :append-icon="isPwdShow ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="isPwdShow ? 'text' : 'password'"
+            label="비밀번호"
+            v-model="user.password"
+            class="input-group--focused"
+            @click:append="isPwdShow = !isPwdShow"
+            @keyup.enter="confirm"
+            color="#289672"
+        ></v-text-field>
+        </v-row>
+        <v-row justify="end" class="mr-5 mb-5">
             <router-link to="/users/findpass" style="color: #289672; text-decoration:none;">
-              비밀번호를 잊으셨나요?
+            비밀번호를 잊으셨나요?
             </router-link>
-          </p>
-        </div>
-      </v-col>
-
-      <v-col cols="12">
-        <div>
-          <v-btn color="rgb(40,150,114)" dark width="35%"> 로그인 </v-btn>
-        </div>
-
-        <br />
-        <div>
-          <p style="font-size: 10px; font-weight: bold">
+        </v-row>
+        
+        <v-row justify="center" class="mb-5 mt-10">
+            <v-btn color="#289672" dark width="35%" @click="confirm"> 로그인 </v-btn>
+        </v-row>
+        <v-row justify="center" class="mb-5">
+            <p style="font-size: 4vw; font-weight: bold">
             계정이 없으신가요?
             <router-link
-              to="/about"
+              to="/users/signup"
               style="color: #289672; text-decoration: none"
             >
               회원가입</router-link
             >
           </p>
-        </div>
-        <br />
-        <div>
-          <!--카카오 로그인 버튼-->
-          <v-btn color="#99C5B9" dark width="80%">
-            <div class="aling-center">
-              <v-icon small>mdi-login</v-icon> 카카오 계정으로 로그인
-            </div>
+        </v-row>
+        <v-row justify="center" class="mb-5">
+            <v-btn color="#99C5B9" dark width="80%">
+                <v-col cols="1">
+              <v-icon small>mdi-login</v-icon></v-col>
+              <v-col cols="11">카카오 계정으로 로그인</v-col>
           </v-btn>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+import {mapState, mapActions} from "vuex";
+
+const userStore = "userStore";
+
 export default {
   name: "LoginView",
+  data(){
+    return{
+      user: {
+        email: null,
+        password: null,
+      },
+      isPwdShow: false,
+    }
+  },
+  computed: {
+    ...mapState(userStore, ["isLogin", "isLoginError"]),
+  },
+  methods: {
+    ...mapActions(userStore, ["userConfirm", "getUserInfo"]),
+    async confirm(){
+      console.log("confirm 들어옴")
+      // 토큰 서버에서 생성 후 저장
+      await this.userConfirm(this.user);
+      // 토큰 받아오기
+      let token = sessionStorage.getItem("Authorization");
+      if(this.isLogin){
+        await this.getUserInfo(token);
+        //console.log(token);
+        this.$router.push({name: "mainList"});
+      }
+    }
+  }
 };
 </script>
+
+<style scoped>
+
+</style>

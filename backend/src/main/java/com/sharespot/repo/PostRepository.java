7a3ca0cb30531fdcf2db79e8e5d36e45c72e4fe4
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-08 17:24:02 +0900

    BE: 유저 게시글리스트 조회기능
    native 쿼리로 전체 엔티티가 아닌 일부컬럼만 가져오도록 구현

--- backend/src/main/java/com/sharespot/repo/PostRepository.java
+++ backend/src/main/java/com/sharespot/repo/PostRepository.java
@@ -4,9 +4,17 @@ package com.sharespot.repo;
 import com.sharespot.entity.Post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    public static final String FIND_USER_POST = "SELECT post_id, image FROM post_table WHERE user_id = :userId";
+
+    @Query(value = FIND_USER_POST, nativeQuery = true)
+    public List<Object[]> findByUserId(int userId);
+
 }

0abb0a5bb0a6d0e08a0bf7d8b24e7d44e91a817e
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-09 23:35:53 +0900

    FIX: 유저 게시물리스트 수정
    유저 마이페이지의 게시물리스트 불러오는 api @Param 추가하여 문제수정

--- backend/src/main/java/com/sharespot/repo/PostRepository.java
+++ backend/src/main/java/com/sharespot/repo/PostRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     public static final String FIND_USER_POST = "SELECT post_id, image FROM post_table WHERE user_id = :userId";
 
     @Query(value = FIND_USER_POST, nativeQuery = true)
-    public List<Object[]> findByUserId(int userId);
+    public List<Object[]> findByUserId(@Param("userId") int userId);
     
     public List<Post> findByUserIdOrderByPostIdDesc(int userId);
     

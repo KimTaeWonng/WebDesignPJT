b9a56f085a57bae824bf1c1bbea3084030f2a6dd
chohyean <ashley3053@gmail.com>
2022-08-11 15:46:57 +0900

    FE: post 작성자이미지 수정 및 댓글등록시간 구현

--- frontend/sharespot/src/components/post/PostCommentItem.vue
+++ frontend/sharespot/src/components/post/PostCommentItem.vue
@@ -1,69 +1,67 @@
 <template>
-    <!-- 개인프로필 표시 -->
-    <v-list-item style="padding:0%;">
-        <!-- :alt="`${chat.title} avatar`"  이거 ${user.username} 이런식으로 불러오기-->
-      <v-col cols="2" allign="center">
+  <!-- 개인프로필 표시 -->
+  <v-list-item style="padding: 0%">
+    <!-- :alt="`${chat.title} avatar`"  이거 ${user.username} 이런식으로 불러오기-->
+    <v-col cols="2" allign="center">
       <v-list-item-avatar>
-          <v-img
-            :src="comment.userImage"
-          ></v-img>
+        <v-img :src="comment.userImage"></v-img>
       </v-list-item-avatar>
-      </v-col>
-      <v-col cols="8" style="padding:4px;">
+    </v-col>
+    <v-col cols="8" style="padding: 4px">
       <div>
-      <v-list-item-content >
-        <!--  이거 ${user.username} 이런식으로 불러오기 + 아래는 위치정보 -->
-        <v-list-item-subtitle style="font-size: 13px; font-weight: 700;">{{ comment.userNick }}</v-list-item-subtitle>
-        <!-- {{ PostComment }} -->
-        <v-list-item-subtitle style="font-size: 12px; font-weight: 700;">{{ comment.comment }}</v-list-item-subtitle>
-        <v-row style="padding: 13px">
-          <v-col style="padding: 0%; font-size: 12px; font-weight: 400;" cols="2">
-            <!-- 시간 데이터 추후에 해야됨 -->
-    
-          </v-col>
-          <v-col style="padding: 0%; font-size: 12px; font-weight: 400; align-items: center;" cols="4">
-            좋아요 1개
-            <!-- 좋아요 {{likecount}} 개 -->
-          </v-col>
-        </v-row>
-      </v-list-item-content>
+        <v-list-item-content>
+          <!--  이거 ${user.username} 이런식으로 불러오기 + 아래는 위치정보 -->
+          <v-list-item-subtitle style="font-size: 11px; font-weight: 700">{{
+            comment.userNick
+          }}</v-list-item-subtitle>
+          <!-- {{ PostComment }} -->
+          <v-list-item-subtitle style="font-size: 12px; font-weight: 700">{{
+            comment.comment
+          }}</v-list-item-subtitle>
+          <v-row style="padding: 13px">
+            <v-col style="padding: 0%; font-size: 11px; font-weight: 400" cols="3">
+              <!-- 시간 데이터 추후에 해야됨 -->
+              {{ comment.uploadTime | moment("from", "now") }}
+            </v-col>
+            <v-col
+              style="padding: 0%; font-size: 11px; font-weight: 400; align-items: center"
+              cols="4"
+            >
+              좋아요 1개
+              <!-- 좋아요 {{likecount}} 개 -->
+            </v-col>
+          </v-row>
+        </v-list-item-content>
       </div>
-      </v-col>
+    </v-col>
 
-      <!-- 옵션 버튼 -->
-      <v-col>
+    <!-- 옵션 버튼 -->
+    <v-col>
       <v-btn @click="showOption()" id="dotBtn" icon>
-        <v-icon color="red" small>
-        mdi-heart-outline
-        </v-icon>
+        <v-icon color="red" small> mdi-heart-outline </v-icon>
       </v-btn>
-      </v-col>
-    </v-list-item>
+    </v-col>
+  </v-list-item>
 </template>
 
 <script>
-
-
 export default {
   data() {
     return {
-      commentItem: []
-    }
+      commentItem: [],
+    };
   },
-  name: 'PostCommentItem',
+  name: "PostCommentItem",
   props: {
-    comment: Array
+    comment: Array,
   },
 
-  methods: {
-    
-  },
-
-}
+  methods: {},
+};
 </script>
 
 <style scoped>
-  #v-chip {
-    color: #289672;
-  }
+#v-chip {
+  color: #289672;
+}
 </style>

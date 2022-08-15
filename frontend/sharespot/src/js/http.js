5137fe5643c45fe90265c4bc7e0fe635dec1c5b6
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-08-15 23:03:55 +0900

    FE: ProfileTaste.vue 함께한시간 추가

--- frontend/sharespot/src/js/http.js
+++ frontend/sharespot/src/js/http.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 // axios 객체 생성
 const http = axios.create({
-    baseURL: "https://localhost:9999/api/",
+    baseURL: "https://i7a505.p.ssafy.io/api/",
     headers: {
         "Content-type": "application/json"
     },
 });
-
 
 export { http };

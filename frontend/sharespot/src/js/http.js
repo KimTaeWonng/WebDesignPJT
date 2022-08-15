e83763b1e0b6e7f6c151da6209618db5301f5802
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-15 20:01:52 +0900

    FE: change http.js
    로컬에서 서버로 변경

--- frontend/sharespot/src/js/http.js
+++ frontend/sharespot/src/js/http.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // axios 객체 생성
 const http = axios.create({
-    baseURL: "https://localhost:9999/api/",
+    // baseURL: "https://localhost:9999/api/",
+    baseURL: "https://i7a505.p.ssafy.io/api/",
     headers: {
         "Content-type": "application/json"
     },

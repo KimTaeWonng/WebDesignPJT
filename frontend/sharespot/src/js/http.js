f8ce4c1a9cf0a851a508b3b07be725e0559aa9fe
김태웅 <85680155+KimTaeWonng@users.noreply.github.com>
2022-08-16 11:49:25 +0900

    FE : 비밀번호 재발급 연동
    버튼이랑 연동시킴

--- frontend/sharespot/src/js/http.js
+++ frontend/sharespot/src/js/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // axios 객체 생성
 const http = axios.create({
     baseURL: "https://i7a505.p.ssafy.io/api/",
+    //baseURL: "https://localhost:9999/api/",
     headers: {
         "Content-type": "application/json"
     },

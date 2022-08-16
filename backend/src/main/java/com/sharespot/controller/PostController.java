e25560fa4a69820ce7feabe84573b6ca83c773e3
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-16 15:45:45 +0900

    FIX: 변수명 수정

--- backend/src/main/java/com/sharespot/controller/PostController.java
+++ backend/src/main/java/com/sharespot/controller/PostController.java
@@ -186,7 +186,7 @@ public class PostController {
 		List<Post> savedPost = new ArrayList<>();
 		
 
-		for(Follow f : followr_list) {
+		for(Follow f : followingList) {
 			Page<Post> post = postRepository.findByUserIdOrderByPostIdDesc(f.getFollowerId(), PageRequest.of(0, 1,Sort.by("postId").descending()));
 			
 			for(Post p :post) {

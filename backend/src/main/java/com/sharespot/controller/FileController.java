42c59a26f0c2b4502b5320d6607250ac20bf7334
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-08-16 21:36:14 +0900

    BE: 파일업로드 수정

--- backend/src/main/java/com/sharespot/controller/FileController.java
+++ backend/src/main/java/com/sharespot/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.sharespot.controller;
 
-import com.sharespot.dto.FileDto;
+import com.sharespot.entity.PostImage;
+import com.sharespot.repo.PostImageRepository;
 import com.sharespot.service.FileService;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
@@ -11,11 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -24,13 +22,13 @@ import java.util.UUID;
 public class FileController {
 
     private final FileService fileService;
-
+    private final PostImageRepository postImageRepository;
 
     @GetMapping(
             produces = MediaType.IMAGE_JPEG_VALUE // content-type
     )
     @ApiOperation(value = "이미지 조회", notes = "imagePath에 해당하는 서버의 이미지 파일을 조회한다.")
-    public ResponseEntity<byte[]> getFile(@RequestParam("imagePath") String imagePath) throws IOException {
+    public ResponseEntity<byte[]> getFile(@RequestParam("imagePath") String imagePath){
 
         byte[] targetArray = null;
         try{
@@ -39,19 +37,40 @@ public class FileController {
             log.debug("이미지 조회 중 에러 : {}", e.getMessage());
         }
 
-        return new ResponseEntity<byte[]>(targetArray, HttpStatus.OK);
+        return new ResponseEntity<>(targetArray, HttpStatus.OK);
     }
 
     @PostMapping
     @ApiOperation(value = "이미지 업로드", notes = "선택된 이미지 파일들을 서버에 업로드한다.")
-    public ResponseEntity<ArrayList<String>> uploadFile(@RequestParam MultipartFile[] files) throws IllegalStateException, IOException {
-        ArrayList result = null;
-        try {
-            result = fileService.uploadImage(files);
-        }catch (Exception e){
-            log.debug("이미지 업로드 중 에러 : {}", e.getMessage());
-        }
+    public ResponseEntity<List<String>> uploadFile(@RequestParam MultipartFile[] files) throws IllegalStateException, IOException {
+        List<String> result = null;
+        result = fileService.uploadImage(files);
+//        try {
+//        }catch (Exception e){
+//            log.debug("이미지 업로드 중 에러 : {}", e.getMessage());
+//        }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
+
+//    @PostMapping("/post")
+//    @ApiOperation(value = "게시판 이미지 업로드", notes = "선택된 이미지 파일들을 서버에 업로드하고 post_image 테이블에 추가한다.")
+//    public ResponseEntity<List<String>> uploadPostFile(@RequestParam MultipartFile[] files, @RequestParam int postId) throws IllegalStateException {
+//        List<String> result = null;
+//        try {
+//            result = fileService.uploadImage(files);
+//
+//            for (String r : result){
+//                PostImage postImage = PostImage.builder()
+//                        .postId(postId)
+//                        .filePath(r)
+//                        .build();
+//                postImageRepository.save(postImage);
+//            }
+//
+//        }catch (Exception e){
+//            log.debug("이미지 업로드 중 에러 : {}", e.getMessage());
+//        }
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
 
 }
